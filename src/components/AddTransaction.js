2c4bd17967aecf0d7adf6e6dba1fc3d450467bfc
Zohaib25 <66442875+Zohaib25@users.noreply.github.com>
2020-06-18 14:00:39 -0700

    Initial committ

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,50 @@
+import React, {useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+
+export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] =useState();
+    const { addTransaction } = useContext(GlobalContext);
+    const onSubmit = e => {
+        e.preventDefault();
+        if (Number(amount) === 0) {
+            alert('Please enter the transaction amount in numbers');
+            return false;
+        }
+        if (text.length === 0){
+            alert('Please enter the required transaction Name')
+            return false;
+        }
+    
+    const newTransaction = {
+        id: Math.floor(Math.random()* 100000000),
+        text,
+        amount: +amount
+    }
+    addTransaction(newTransaction);
+    setAmount('');
+    setText('');
+    }
+    return (
+        <>
+           <h3>Add new Transaction</h3> 
+           <form onSubmit={onSubmit}> 
+           <div className="form-control"> 
+            <label htmlFor="text">Text</label>
+            <input type="text"  value={text} onChange={(e)=>{setText(e.target.value)}}
+             placeholder="Enter text..."></input>
+
+           </div>
+           <div className="form-control">
+               <label htmlFor="amount">Amount <br/>
+               (negative -expense, positive -income)</label>
+               <input type="amount" value={amount} onChange={(e)=>{setAmount(e.target.value)}} 
+               placeholder="Enter amount..."></input>
+
+           </div>
+           <button className="btn">Add transaction</button>
+           </form>
+        </>
+    )
+}
---
+++ src/context/GlobalState.js
@@ -0,0 +1,50 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+// Initial State
+
+const initialState = {
+    transactions: []
+}
+//{ id: 1, text:'Flower', amount: -20},
+        //{ id: 2, text: 'Burger', amount: -40},
+        //{ id:3, text: 'Earning', amount:+500},
+        //{ id:4, text:'Earning 1', amount: +200}
+// Create Context
+
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) =>{
+ const [state, dispatch] = useReducer(AppReducer, initialState);
+
+ // Actions
+
+ function deleteTransaction(id) {
+     dispatch({
+         type: 'DELETE_TRANSACTION',
+         payload: id
+     }
+ 
+
+     );
+ }
+ function addTransaction(transaction) {
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transaction
+    }
+
+
+    );
+}
+
+ return (<GlobalContext.Provider value={{
+     transactions: state.transactions,
+     deleteTransaction,
+     addTransaction
+ }
+
+ }>
+     {children}
+ </GlobalContext.Provider>);
+}
